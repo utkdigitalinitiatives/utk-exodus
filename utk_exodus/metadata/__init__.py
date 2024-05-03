5452a99b5f6a64f6c9fabc3a33d6621e673f688d
Weston49 <weston.agreda@gmail.com>
2024-05-03 13:13:26 -0400

    added locations test

--- utk_exodus/metadata/__init__.py
+++ utk_exodus/metadata/__init__.py
@@ -1,2 +1,2 @@
-from .metadata import ExtentProperty, GeoNamesProperty, MetadataMapping, NameProperty, TitleProperty
-__all__ = ['ExtentProperty', 'GeoNamesProperty', 'MetadataMapping', 'NameProperty', 'TitleProperty']
+from .metadata import ExtentProperty, GeoNamesProperty, MetadataMapping, NameProperty, PhysicalLocationsProperties, TitleProperty
+__all__ = ['ExtentProperty', 'GeoNamesProperty', 'MetadataMapping', 'NameProperty', 'PhysicalLocationsProperties', 'TitleProperty']
