956ddc8a7130c680d9283046514c3c0cef84a4de
Weston49 <weston.agreda@gmail.com>
2024-05-06 15:02:06 -0400

    adds several unit tests

---
+++ tests/test_metadata_rights_license.py
@@ -0,0 +1,33 @@
+import pytest
+from utk_exodus.metadata import RightsOrLicenseProperties
+from pathlib import Path
+
+fixtures_path = Path(__file__).parent / "fixtures"
+
+NAMESPACES = {
+    'mods': 'http://www.loc.gov/mods/v3', 
+    'xlink': 'http://www.w3.org/1999/xlink'
+}
+
+@pytest.fixture(
+    params=[
+        {"filename": "heilman:1010.xml",
+         "expected_results": {
+            'license': ['http://creativecommons.org/licenses/by-nc-nd/3.0/'], 'rights_statement': ['http://rightsstatements.org/vocab/InC/1.0/']
+            }
+        },
+        {"filename": "civilwar_1438.xml",
+         "expected_results": {
+            'rights_statement': ['http://rightsstatements.org/vocab/NoC-US/1.0/']
+            }
+        }
+    ]
+)
+def fixture(request):
+    request.param["fixtures_path"] = fixtures_path / request.param.get("filename")
+    return request.param
+
+def test_find_method_on_locations(fixture):
+    rights = RightsOrLicenseProperties(fixture.get("fixtures_path"), NAMESPACES)
+    results = rights.find()
+    assert results == fixture["expected_results"]
