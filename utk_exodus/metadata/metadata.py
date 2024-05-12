7838438c2024e6b6f45ddd63b830e2611321f810
Mark Baggett <mbagget1@utk.edu>
2024-05-11 22:08:48 -0400

    Remove case switch to support back to 3.8.

--- utk_exodus/metadata/metadata.py
+++ utk_exodus/metadata/metadata.py
@@ -832,11 +832,28 @@ class LanguageURIProperty(BaseProperty):
 
 
 class ExtentProperty(BaseProperty):
+    """
+    Class to Handle Extent Information.
+
+    Args:
+        path (str): The path to the file.
+        namespaces (dict): Namespaces to be used in the XPath queries.
+    """
     def __init__(self, path: str, namespaces: dict):
         super().__init__(path, namespaces)
 
     def find(self):
-        """Finds and returns a dictionary containing the extent information of a record."""
+        """
+        Finds and returns a dictionary containing the extent information of a record.
+
+        Returns:
+            dict: A dictionary containing the extent information.
+
+        Examples:
+            >>> extent_property = ExtentProperty("knoxgardens_125.xml", {"mods": "http://www.loc.gov/mods/v3"})
+            {"extent": ["3 1/4 x 5 inches"]}
+
+        """
         extents_without_units = [
             text.text
             for text in self.root.xpath(
@@ -981,39 +998,41 @@ class MetadataMapping:
 
     @staticmethod
     def __lookup_special_property(special_property, file, namespaces, name):
-        match special_property:
-            case "TitleProperty":
-                return TitleProperty(file, namespaces).find()
-            case "NameProperty":
-                return NameProperty(file).find()
-            case "RoleAndNameProperty":
-                return RoleAndNameProperty(file).find()
-            case "GeoNamesProperty":
-                return GeoNamesProperty(file, namespaces).find(name)
-            case "DataProvider":
-                return DataProvider(file, namespaces).find()
-            case "PhysicalLocationsProperties":
-                return PhysicalLocationsProperties(file, namespaces).find()
-            case "SubjectProperty":
-                return SubjectProperty(file, namespaces).find_topic()
-            case "KeywordProperty":
-                return KeywordProperty(file, namespaces).find_topic()
-            case "TypesProperties":
-                return TypesProperties(file, namespaces).find()
-            case "LocalTypesProperties":
-                return LocalTypesProperties(file, namespaces).find()
-            case "LanguageURIProperty":
-                return LanguageURIProperty(file, namespaces).find_term()
-            case "PublisherProperty":
-                return PublisherProperty(file, namespaces).find()
-            case "PublicationPlaceProperty":
-                return PublicationPlaceProperty(file, namespaces).find()
-            case "RightsOrLicenseProperties":
-                return RightsOrLicenseProperties(file, namespaces).find()
-            case "ExtentProperty":
-                return ExtentProperty(file, namespaces).find()
-            case "MachineDate":
-                return MachineDate(file, namespaces).find()
+        if special_property == "TitleProperty":
+            return TitleProperty(file, namespaces).find()
+        elif special_property == "NameProperty":
+            return NameProperty(file).find()
+        elif special_property == "RoleAndNameProperty":
+            return RoleAndNameProperty(file).find()
+        elif special_property == "GeoNamesProperty":
+            return GeoNamesProperty(file, namespaces).find(name)
+        elif special_property == "DataProvider":
+            return DataProvider(file, namespaces).find()
+        elif special_property == "PhysicalLocationsProperties":
+            return PhysicalLocationsProperties(file, namespaces).find()
+        elif special_property == "SubjectProperty":
+            return SubjectProperty(file, namespaces).find_topic()
+        elif special_property == "KeywordProperty":
+            return KeywordProperty(file, namespaces).find_topic()
+        elif special_property == "TypesProperties":
+            return TypesProperties(file, namespaces).find()
+        elif special_property == "LocalTypesProperties":
+            return LocalTypesProperties(file, namespaces).find()
+        elif special_property == "LanguageURIProperty":
+            return LanguageURIProperty(file, namespaces).find_term()
+        elif special_property == "PublisherProperty":
+            return PublisherProperty(file, namespaces).find()
+        elif special_property == "PublicationPlaceProperty":
+            return PublicationPlaceProperty(file, namespaces).find()
+        elif special_property == "RightsOrLicenseProperties":
+            return RightsOrLicenseProperties(file, namespaces).find()
+        elif special_property == "ExtentProperty":
+            return ExtentProperty(file, namespaces).find()
+        elif special_property == "MachineDate":
+            return MachineDate(file, namespaces).find()
+        else:
+            # Handle unknown special property
+            raise ValueError(f"Unknown special property: {special_property}")
 
     def write_csv(self, filename):
         with open(filename, "w", newline="") as bulkrax_sheet:
