dec77b030a2b16f54ba3d60f8d291503fb691707
Mark Baggett <mbagget1@utk.edu>
2024-05-16 08:55:39 -0400

    Add failing test.

---
+++ tests/test_metadata_subject.py
@@ -0,0 +1,44 @@
+import pytest
+from utk_exodus.metadata import SubjectProperty
+from pathlib import Path
+
+# Set path to fixtures
+fixtures_path = Path(__file__).parent / "fixtures"
+
+# Set namespaces
+NAMESPACES = {
+    "mods": "http://www.loc.gov/mods/v3",
+    "xlink": "http://www.w3.org/1999/xlink",
+}
+
+
+@pytest.fixture(
+    params=[
+        {
+            "filename": "cdf_13238.xml",
+            "expected_results": {
+                "subject": [
+                    "http://id.loc.gov/authorities/subjects/sh85023396 ",
+                    "http://id.loc.gov/authorities/subjects/sh87000100",
+                    "http://id.loc.gov/authorities/names/n79059917",
+                ]
+            },
+        },
+        {
+            "filename": "egypt_224.xml",
+            "expected_results": {
+                "subject": ["http://id.loc.gov/authorities/subjects/sh85016233"]
+            },
+        },
+    ]
+)
+def fixture(request):
+    param = request.param
+    param["fixture_path"] = fixtures_path / param.get("filename")
+    return param
+
+
+def test_find_method_on_subject(fixture):
+    subjects = SubjectProperty(fixture.get("fixture_path"), NAMESPACES)
+    results = subjects.find_topic()
+    assert results == fixture["expected_results"]
